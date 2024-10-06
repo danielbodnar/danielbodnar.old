102bdab17f90c5b447ec8f67d2b4352a5776b58c
Daniel Bodnar <1790726+danielbodnar@users.noreply.github.com>
2024-10-06 17:22:17 -0500

    init

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,25 +1,26 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  extends: ['@nuxt/content'],
-  modules: [
-    '@nuxt/content',
-    '@nuxtjs/tailwindcss',
-  ],
-  content: {
-    highlight: {
-      theme: 'github-dark'
-    }
-  },
-  app: {
-    head: {
-      title: 'Daniel Bodnar - SRE / Infrastructure Engineer',
-      meta: [
-        { charset: 'utf-8' },
-        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-        { hid: 'description', name: 'description', content: 'Professional portfolio of Daniel Bodnar, SRE / Infrastructure Engineer / Solutions Architect / Linux Evangelist' }
-      ],
-      link: [
-        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-      ]
-    }
-  }
-})
+  modules: ["@nuxt/content", "@nuxtjs/tailwindcss"],
+  // content: {
+  //   highlight: {
+  //     theme: "github-dark",
+  //   },
+  // },
+  // app: {
+  //   head: {
+  //     title: "Daniel Bodnar - SRE / Infrastructure Engineer",
+  //     meta: [
+  //       { charset: "utf-8" },
+  //       { name: "viewport", content: "width=device-width, initial-scale=1" },
+  //       {
+  //         hid: "description",
+  //         name: "description",
+  //         content:
+  //           "Professional portfolio of Daniel Bodnar, SRE / Infrastructure Engineer / Solutions Architect / Linux Evangelist",
+  //       },
+  //     ],
+  //     link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+  //   },
+  // },
+  devtools: { enabled: true },
+});
